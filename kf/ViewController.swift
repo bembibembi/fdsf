b665ab7641ce282d28bd2e427aa16db94b62314d
bembi <ismanovvvabembi123>
2023-01-09 15:57:28 +0600

    Initial Commit

---
+++ kf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kf
+//
+//  Created by 지구9 on 9/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
